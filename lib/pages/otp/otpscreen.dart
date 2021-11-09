dba83c6d474af351cd6f52b834e1ca3fc0f24a14
Wahyu Hidayat <whyhid47@gmail.com>
2021-11-09 13:13:07 +0700

    update ui

---
+++ lib/pages/otp/otpscreen.dart
@@ -0,0 +1,168 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lk21/utils/const.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+class OtpScreen extends StatefulWidget {
+  const OtpScreen({Key? key}) : super(key: key);
+
+  @override
+  _OtpScreenState createState() => _OtpScreenState();
+}
+
+class _OtpScreenState extends State<OtpScreen> {
+  TextEditingController textEditingController = TextEditingController();
+  // ..text = "123456";
+
+  // ignore: close_sinks
+  StreamController<ErrorAnimationType>? errorController;
+
+  bool hasError = false;
+  String currentText = "";
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    errorController = StreamController<ErrorAnimationType>();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    errorController!.close();
+
+    super.dispose();
+  }
+
+  // snackBar Widget
+  snackBar(String? message) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message!),
+        duration: const Duration(seconds: 2),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Image.asset(
+                      patternSplash,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30, left: 40),
+                    child: InkWell(
+                      onTap: () {
+                        Get.back();
+                      },
+                      child: SizedBox(
+                          width: 45, height: 45, child: Image.asset(icback)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 100, left: 40),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text("Enter 4-digit\nVerification code",
+                            style: fontDefault.copyWith(
+                                fontSize: 25,
+                                color: whiteColor,
+                                fontWeight: FontWeight.bold)),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                            "Code send to +62812000**** .\nThis code will expired in 01:30",
+                            style: fontDefault.copyWith(
+                                fontSize: 14,
+                                color: whiteColor,
+                                fontWeight: FontWeight.normal)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Form(
+                child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 50),
+                    child: PinCodeTextField(
+                      appContext: context,
+                      pastedTextStyle: const TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      length: 4,
+                      obscureText: true,
+                      obscuringCharacter: '*',
+                      blinkWhenObscuring: true,
+                      animationType: AnimationType.fade,
+                      validator: (v) {
+                        if (v!.length < 4) {
+                          return "I'm from validator";
+                        } else {
+                          return null;
+                        }
+                      },
+                      pinTheme: PinTheme(
+                          shape: PinCodeFieldShape.box,
+                          borderRadius: BorderRadius.circular(5),
+                          fieldHeight: 50,
+                          fieldWidth: 40,
+                          activeFillColor: Colors.white),
+                      cursorColor: Colors.black,
+                      animationDuration: const Duration(milliseconds: 300),
+                      enableActiveFill: true,
+                      errorAnimationController: errorController,
+                      controller: textEditingController,
+                      keyboardType: TextInputType.number,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      boxShadows: [
+                        const BoxShadow(
+                          offset: Offset(0, 1),
+                          color: Colors.black12,
+                          blurRadius: 10,
+                        )
+                      ],
+                      onCompleted: (v) {
+                        print("Completed");
+                      },
+                      onChanged: (value) {
+                        print(value);
+                        setState(() {
+                          currentText = value;
+                        });
+                      },
+                      beforeTextPaste: (text) {
+                        print("Allowing to paste $text");
+                        //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+                        //but you can show anything you want here, like your pop up saying wrong paste format or etc
+                        return true;
+                      },
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/success/signup_success.dart
+++ lib/pages/success/signup_success.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lk21/pages/otp/otpscreen.dart';
 import 'package:lk21/utils/const.dart';
 
 class SignUpSuccess extends StatefulWidget {
@@ -13,9 +15,55 @@ class _SignUpSuccessState extends State<SignUpSuccess> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: SizedBox(
-        width: double.infinity,
-        height: MediaQuery.of(context).size.height,
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Image.asset(success, fit: BoxFit.cover),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Congrats!",
+                style: fontDefault.copyWith(
+                    color: primaryButton,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 26),
+              ),
+              Text(
+                "Your profile is ready to use",
+                style: fontDefault.copyWith(
+                    color: whiteColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: ElevatedButton(
+                    onPressed: () {
+                      Get.to(const OtpScreen());
+                    },
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        primary: primaryButton,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 50, vertical: 20)),
+                    child: Text(
+                      "Try Order",
+                      style: fontDefault.copyWith(
+                          color: whiteColor, fontWeight: FontWeight.bold),
+                    )),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
